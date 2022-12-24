2cd92023005f0f1a026a684825921fc8781f0ac6
olenasniatynska <92575026+olenasniatynska@users.noreply.github.com>
2022-12-24 21:34:04 +0200

    Create Test.java

---
+++ src/main/Test.java
@@ -0,0 +1,13 @@
+package task1.lab11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
